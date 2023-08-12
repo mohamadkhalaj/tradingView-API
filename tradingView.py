213a201c59ccf8c5b31b70d90aa2507da6fb1112
MohammadMahdi <mohammadkhalaj@outlook.com>
2023-08-12 14:58:41 +0330

    Add volume and change value.

--- tradingView.py
+++ tradingView.py
@@ -7,12 +7,12 @@ import requests
 from websocket import create_connection
 
 
-def search(query, type):
-    # type = 'stock' | 'futures' | 'forex' | 'cfd' | 'crypto' | 'index' | 'economic'
+def search(query, category):
+    # category = 'stock' | 'futures' | 'forex' | 'cfd' | 'crypto' | 'index' | 'economic'
     # query = what you want to search!
     # it returns first matching item
     res = requests.get(
-        f"https://symbol-search.tradingview.com/symbol_search/?text={query}&type={type}"
+        f"https://symbol-search.tradingview.com/symbol_search/?text={query}&type={category}"
     )
     if res.status_code == 200:
         res = res.json()
@@ -23,53 +23,58 @@ def search(query, type):
         exit(1)
 
 
-def generateSession():
-    stringLength = 12
+def generate_session():
+    string_length = 12
     letters = string.ascii_lowercase
-    random_string = "".join(random.choice(letters) for i in range(stringLength))
+    random_string = "".join(random.choice(letters) for _ in range(string_length))
     return "qs_" + random_string
 
 
-def prependHeader(st):
-    return "~m~" + str(len(st)) + "~m~" + st
+def prepend_header(content):
+    return f"~m~{len(content)}~m~{content}"
 
 
-def constructMessage(func, paramList):
-    return json.dumps({"m": func, "p": paramList}, separators=(",", ":"))
+def construct_message(func, param_list):
+    return json.dumps({"m": func, "p": param_list}, separators=(",", ":"))
 
 
-def createMessage(func, paramList):
-    return prependHeader(constructMessage(func, paramList))
+def create_message(func, param_list):
+    return prepend_header(construct_message(func, param_list))
 
 
-def sendMessage(ws, func, args):
-    ws.send(createMessage(func, args))
+def send_message(ws, func, args):
+    ws.send(create_message(func, args))
 
 
-def sendPingPacket(ws, result):
-    pingStr = re.findall(".......(.*)", result)
-    if len(pingStr) != 0:
-        pingStr = pingStr[0]
-        ws.send("~m~" + str(len(pingStr)) + "~m~" + pingStr)
+def send_ping_packet(ws, result):
+    ping_str = re.findall(".......(.*)", result)
+    if ping_str:
+        ping_str = ping_str[0]
+        ws.send(f"~m~{len(ping_str)}~m~{ping_str}")
 
 
-def socketJob(ws):
+def socket_job(ws):
     while True:
         try:
             result = ws.recv()
             if "quote_completed" in result or "session_id" in result:
                 continue
-            Res = re.findall("^.*?({.*)$", result)
-            if len(Res) != 0:
-                jsonRes = json.loads(Res[0])
-
-                if jsonRes["m"] == "qsd":
-                    symbol = jsonRes["p"][1]["n"]
-                    price = jsonRes["p"][1]["v"]["lp"]
-                    print(f"{symbol} -> {price}")
+            res = re.findall("^.*?({.*)$", result)
+            if res:
+                json_res = json.loads(res[0])
+                if json_res["m"] == "qsd":
+                    prefix = json_res["p"][1]
+                    symbol = prefix["n"]
+                    price = prefix["v"].get("lp", None)
+                    volume = prefix["v"].get("volume", None)
+                    change = prefix["v"].get("ch", None)
+                    change_percentage = prefix["v"].get("chp", None)
+                    print(
+                        f"{symbol} -> {price=}, {change=}, {change_percentage=}, {volume=}"
+                    )
             else:
                 # ping packet
-                sendPingPacket(ws, result)
+                send_ping_packet(ws, result)
         except KeyboardInterrupt:
             print("\nGoodbye!")
             exit(0)
@@ -78,36 +83,42 @@ def socketJob(ws):
             continue
 
 
-def getSymbolId(pair, market):
+def get_symbol_id(pair, market):
     data = search(pair, market)
     symbol_name = data["symbol"]
-    try:
-        broker = data["prefix"]
-    except KeyError:
-        broker = data["exchange"]
+    broker = data.get("prefix", data["exchange"])
     symbol_id = f"{broker.upper()}:{symbol_name.upper()}"
     print(symbol_id, end="\n\n")
     return symbol_id
 
 
 def main(pair, market):
-
-    # serach btcusdt from crypto category
-    symbol_id = getSymbolId(pair, market)
+    # search btcusdt from crypto category
+    symbol_id = get_symbol_id(pair, market)
 
     # create tunnel
-    tradingViewSocket = "wss://data.tradingview.com/socket.io/websocket"
+    trading_view_socket = "wss://data.tradingview.com/socket.io/websocket"
     headers = json.dumps({"Origin": "https://data.tradingview.com"})
-    ws = create_connection(tradingViewSocket, headers=headers)
-    session = generateSession()
+    ws = create_connection(trading_view_socket, headers=headers)
+    session = generate_session()
 
     # Send messages
-    sendMessage(ws, "quote_create_session", [session])
-    sendMessage(ws, "quote_set_fields", [session, "lp"])
-    sendMessage(ws, "quote_add_symbols", [session, symbol_id])
-
+    send_message(ws, "quote_create_session", [session])
+    send_message(
+        ws,
+        "quote_set_fields",
+        [
+            session,
+            "lp",
+            "low_price",
+            "volume",
+            "ch",
+            "chp",
+        ],
+    )
+    send_message(ws, "quote_add_symbols", [session, symbol_id])
     # Start job
-    socketJob(ws)
+    socket_job(ws)
 
 
 if __name__ == "__main__":
